8872de3982d4ee3b96439b9a5f7499d5880edf3f
Zurab Zaridze <zaridzezuri@gmail.com>
2023-04-25 23:48:45 +0400

    client&server: implement basic question-answer-winners functionality

--- client/app/components/app/game/Game.js
+++ client/app/components/app/game/Game.js
@@ -1,8 +1,63 @@
-export default function Game() {
+import { useEffect, useState } from "react";
+import { useSocketConnection } from "@/app/hooks/useSocketConnection";
 
-    return (
-        <div>
-            Game here
-        </div>
-    )
+export default function Game() {
+  const [warmupTimer, setWarmupTimer] = useState(null);
+  const [gameTimer, setGameTimer] = useState(null);
+  const [gameOver, setGameOver] = useState(false);
+  const [winners, setWinners] = useState([]);
+
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
+
+  const [input, setInput] = useState("");
+
+  const socket = useSocketConnection();
+
+  useEffect(() => {
+    socket?.on("connect", () => {
+      console.log("connect good");
+
+      socket?.on("warmup-timer", (data) => setWarmupTimer(data || null));
+      socket?.on("game-timer", (data) => setGameTimer(data || null));
+      socket?.on("game-over", (data) => {
+        setGameOver(true);
+        setWinners(data);
+      });
+
+      socket?.on("question", (question, answer) => {
+        setQuestion(question);
+        setAnswer(answer);
+      });
+    });
+  }, [socket]);
+
+  useEffect(() => {
+    socket?.emit("answer", input);
+  }, [input]);
+
+  return (
+    <div>
+      Game here
+      {warmupTimer && <h1>Game starts in: {warmupTimer}</h1>}
+      {gameTimer && (
+        <>
+          <h1>Game Has Started {gameTimer}</h1>
+
+          <h2>Question: {question}</h2>
+
+          <input value={input} onChange={(e) => setInput(e.target.value)} />
+        </>
+      )}
+      {gameOver && (
+        <>
+          <h1>Game Has Ended</h1>
+
+          {winners?.map((winner, id) => {
+            return <h5 key={id}>Winner: {winner}</h5>;
+          })}
+        </>
+      )}
+    </div>
+  );
 }
--- server/index.js
+++ server/index.js
@@ -12,26 +12,20 @@ const io = new Server(httpServer, {
 });
 
 const MAX_PLAYERS = 2;
+const WARMUP_TIME = 6;
+const GAME_TIME = 14;
+const QUESTION = "32*32";
+const ANSWER = "1024";
 
-function startGameTimer() {
-  // Start the game length timer
-  const gameDuration = 10; // in seconds
-  let remainingGameTime = gameDuration;
-  const gameInterval = setInterval(() => {
-    remainingGameTime--;
-    io.emit("game-timer", remainingGameTime);
-
-    if (remainingGameTime === 0) {
-      clearInterval(gameInterval);
-      io.emit("game-over");
-    }
-  }, 1000);
-}
+let winners = [];
 
 function startGame() {
   console.log("Game has started");
 
-  const warmupDuration = 5; // in seconds
+  // reset winners
+  winners = [];
+
+  const warmupDuration = WARMUP_TIME; // in seconds
   let remainingWarmupTime = warmupDuration;
   const warmupInterval = setInterval(() => {
     remainingWarmupTime--;
@@ -40,12 +34,44 @@ function startGame() {
     if (remainingWarmupTime === 0) {
       clearInterval(warmupInterval);
 
-      startGameTimer();
+      // Start the game length timer
+      const gameDuration = GAME_TIME; // in seconds
+      let remainingGameTime = gameDuration;
+      const gameInterval = setInterval(() => {
+        remainingGameTime--;
+        io.emit("game-timer", remainingGameTime);
+
+        if (remainingGameTime === 0) {
+          clearInterval(gameInterval);
+          io.emit("game-over", winners);
+        }
+      }, 1000);
+
+      io.emit("question", QUESTION, ANSWER);
     }
   }, 1000);
 }
 
 io.on("connection", (socket) => {
+  // Handle user answers during started game
+  socket.on("answer", (answer) => {
+    if (answer == ANSWER) {
+      // add to list
+      winners.push(socket.id);
+
+      console.log("added", winners);
+    } else {
+      // remove from list
+
+      const index = winners.indexOf(socket.id);
+      if (index > -1) {
+        winners.splice(index, 1);
+
+        console.log("spliced", winners);
+      }
+    }
+  });
+
   socket.on("join-room", (room) => {
     socket.join(room);
 
@@ -56,7 +82,7 @@ io.on("connection", (socket) => {
     if ([...userQuantity].length == MAX_PLAYERS) {
       io.emit("startGame", true, [...userQuantity]);
 
-      startGame(room);
+      startGame();
     } else {
       io.emit("startGame", false, [...userQuantity]);
     }
